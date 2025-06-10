188e562e8c1e23848bb3fadf91caa92c10525549
aboerstra <136114280+aboerstra@users.noreply.github.com>
2025-06-10 15:30:01 -0400

    Set Vite base to /portfoliomgr/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react(),tailwindcss()],
+  base: '/portfoliomgr/',
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
